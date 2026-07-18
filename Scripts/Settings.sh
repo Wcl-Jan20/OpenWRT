3d143081f182bc76da033c73dcecbe371bd255e3
dep0rt <9974648+77160860@users.noreply.github.com>
2026-07-18 15:35:44 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -70,20 +70,16 @@ rm -rf feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/stat
 sed -i 's/ECM://g' target/linux/qualcommax/base-files/sbin/cpuusage
 sed -i 's/HWE/NPU/g' target/linux/qualcommax/base-files/sbin/cpuusage
 
-#关闭DNS重定向及缓存
+#关闭重绑定保护及缓存
 sed -i 's/option rebind_protection 1/option rebind_protection 0/g' package/network/services/dnsmasq/files/dhcp.conf
 sed -i 's/8000/0/g' package/network/services/dnsmasq/files/dhcp.conf
 
 #去掉luci版本后缀
 sed -i "s#_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || ''),#_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description : ''),#g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 
-#开启zram的lz4默认算法并禁用开机自启
+#禁用zram开机自启
 if [ -f "include/rootfs.mk" ]; then
-    sed -i '/\$(call prepare_rootfs,\$(1),\$(ROOTFS_KILL))/a \	rm -f \$(1)/etc/rc.d/S15zram' include/rootfs.mk
-fi
-if [ -f "package/system/zram-swap/files/zram.init" ]; then
-    sed -i '/# default to lzo, which is always available/{n;s/"lzo"/"lz4"/;}' \
-      package/system/zram-swap/files/zram.init
+    sed -i '/clean_ipkg,\$(1)/a \	rm -f \$(1)/etc/rc.d/S15zram' include/rootfs.mk
 fi
 
 # 禁用 mtk、rk、x86 平台的 sqm nss
