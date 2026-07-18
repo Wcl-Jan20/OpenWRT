b76c3aef4fc61c36e895d7b282ba01c4771a3c81
dep0rt <9974648+77160860@users.noreply.github.com>
2026-07-18 20:19:28 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -76,6 +76,9 @@ fi
 sed -i 's/option rebind_protection 1/option rebind_protection 0/g' package/network/services/dnsmasq/files/dhcp.conf
 sed -i 's/8000/0/g' package/network/services/dnsmasq/files/dhcp.conf
 
+#去掉luci版本后缀
+sed -i "s#_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || ''),#_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description : ''),#g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+
 #禁用zram开机自启
 if [ -f "include/rootfs.mk" ]; then
     sed -i '/clean_ipkg,\$(1)/a \	rm -f \$(1)/etc/rc.d/S15zram' include/rootfs.mk
