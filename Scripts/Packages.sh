c3674d30332f512a72786bc18df42f43fd3d7217
dep0rt <9974648+77160860@users.noreply.github.com>
2026-09-22 23:49:50 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -70,6 +70,7 @@ UPDATE_PACKAGE "timecontrol" "sirpdboy/luci-app-timecontrol" "main"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "luci-app-gecoosac" "laipeng668/luci-app-gecoosac" "main"
 
+UPDATE_PACKAGE "sing" "77160860/luci-app-sing" "main"
 UPDATE_PACKAGE "sing-box" "77160860/OpenWRT" "main" "pkg"
 UPDATE_PACKAGE "mihomo" "77160860/OpenWRT" "main" "pkg"
 rm -rf ./OpenWrt-nikki/mihomo-meta ./OpenWrt-nikki/mihomo-alpha
